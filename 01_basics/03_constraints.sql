73f41678a94343e8a6f4426fcb8db8d0c7279a3a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-02 21:54:25 +0530

    commit

---
+++ 01_basics/03_constraints.sql
@@ -0,0 +1,8 @@
+USE college;
+
+Create TABLE temp1(
+id INT,
+name VARCHAR(50) NOT NULL,
+age INT NOT NULL,
+PRIMARY KEY(id)
+);
---
+++ 02_basics_constraints/constraints.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE college2;
+USE college2;
+
+CREATE TABLE student(
+rollNo INT PRIMARY KEY,
+name VARCHAR(50),
+marks INT ,
+grade VARCHAR(5),
+city VARCHAR(20)
+);

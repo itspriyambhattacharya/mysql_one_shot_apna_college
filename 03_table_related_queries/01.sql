ae284d4133f61297c9a3b041e3b21fa36c1a25ea
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-03 11:44:55 +0530

    added update and delete queury

--- 03_table_related_queries/01.sql
+++ 03_table_related_queries/01.sql
@@ -5,3 +5,12 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE student
 SET grade = "O"
 WHERE grade = "A";
+
+SELECT *
+FROM student;
+
+UPDATE student
+SET marks = marks +1;
+
+DELETE FROM student
+WHERE marks < 33;

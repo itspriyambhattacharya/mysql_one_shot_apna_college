275205148f4f77fd47de987614a8700c7982f61b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-03 20:11:59 +0530

    added some more sub queries

--- 06_sub_query/01_subQuery.sql
+++ 06_sub_query/01_subQuery.sql
@@ -7,3 +7,19 @@ WHERE marks > (
 SELECT AVG(marks)
 FROM student
 );
+
+# Find names of all students with even roll number.
+SELECT name, rollNo
+FROM student
+WHERE  rollNo % 2 =0;
+
+SELECT *
+FROM student;
+
+# Find max marks from students of Delhi.
+SELECT MAX(marks)
+FROM student
+WHERE city = "Delhi";
+
+SELECT MAX(marks)
+FROM (SELECT * FROM student WHERE city = "Delhi") temp;
